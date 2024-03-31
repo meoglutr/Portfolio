5e0a32fec19ed8b192a8ccb5028130ee558ec288
Mehmet Elifoğlu <mehmeteoglutr@gmail.com>
2024-04-01 01:55:43 +0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./componenets/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
+
   return (
     <>
-      <Header />
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      < Header />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route index path="/about" element={<About />} />
+        <Route index path="/projects" element={<Projects />} />
+      </Routes>
     </>
   );
 }
--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   let links = [
-    { name: "Anasayfa" },
-    { name: "Hakkımda" },
-    { name: "Cv İndir" },
+    { name: "Anasayfa", href: "/" },
+    { name: "Hakkımda", href: "/about" },
+    { name: "Projelerim", href: "/projects" },
+    { name: "Cv İndir", href: "" },
   ];
+
   return (
-    <div className="bg-blue p-5 text-white">
+    <div className="bg-blue p-5">
       <div className="flex justify-between">
-        <h1>Mehmet Elifoğlu</h1>
+        <h1 className="text-white">Mehmet Elifoğlu</h1>
         <ul className="flex">
-          {links.map((link, key) => {
-            <li key={key}>{link.name}</li>;
-          })}
+          {links.map((link, index) => (
+            <Link key={index} to={link.href}>
+              <li className="text-white m-1">{link.name}</li>
+            </Link>
+          ))}
         </ul>
       </div>
     </div>
