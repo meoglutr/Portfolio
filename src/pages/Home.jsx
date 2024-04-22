55f2a94fdb90860ea7210262b668309185148a5d
Mehmet Elifoğlu <mehmeteoglutr@gmail.com>
2024-04-22 21:42:11 +0300

    -

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import pdfUrl from "../Mehmet_Elifoglu_Cv.pdf";
 function Home() {
   return (
     <>
-      <div className="flex items-center justify-center ">
+      <div className="flex items-center justify-center  m-10">
         <div className="w-[595px] h-[842px] flex items-center justify-center bg-white m-3">
         <iframe width="100%" height="100%" src={pdfUrl}></iframe>
         </div>
